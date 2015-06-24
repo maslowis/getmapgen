0c5df0333dc45aa30bf3b975a3b4799a282bbfcc
maslowis <iwan.maslow@gmail.com>
2015-06-24 17:48:29 +0600

    Changed log appender, application log is created each time the application starts.

--- src/main/java/com/github/getmapgen/Main.java
+++ src/main/java/com/github/getmapgen/Main.java
@@ -2,12 +2,21 @@ package com.github.getmapgen;
 
 import com.github.getmapgen.view.Form;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Initializer for application
  *
  * @author maslowis
  */
 public class Main {
+
+    static {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+        System.setProperty("current.date", dateFormat.format(new Date()));
+    }
+
     public static void main(String[] args) {
         Form gui = new Form();
     }
