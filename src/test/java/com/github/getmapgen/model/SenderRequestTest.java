d18468605854f85b8c39a6e93e6b02f02b2252e6
maslowis <iwan.maslow@gmail.com>
2015-06-24 16:48:04 +0600

    small refactoring for test

--- src/test/java/com/github/getmapgen/model/SenderRequestTest.java
+++ src/test/java/com/github/getmapgen/model/SenderRequestTest.java
@@ -9,6 +9,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -18,17 +19,14 @@ public class SenderRequestTest {
     @InjectMocks
     private SenderRequest sender;
 
-    private String url;
-
     @BeforeMethod
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        url = "http://www.yandex.ru/";
     }
 
     @Test
     public void testSend() throws Exception {
-        sender.send(url);
+        sender.send(anyString());
         verify(client, times(1)).execute(any(HttpGet.class));
     }
 }
